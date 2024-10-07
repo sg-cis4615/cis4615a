42ff4201277351252abc4ed718ae3d382205059a
sophia <sophiagnc@gmail.com>
2024-10-06 20:57:32 -0400

    update code to be compliant

--- R49_MSC03_J.java
+++ R49_MSC03_J.java
@@ -1,11 +1,27 @@
 //Rule 49 Miscellaneous 
 //MSC03-J Never hard code sensitive information
 
-//Non-Compliant Code:
+//Compliant Code:
 //...
 class IPaddress {
-    String ipAddress = new String("172.16.254.1");
-    public static void main(String[] args) {
-        //...
+    public static void main(String[] args) throws IOException {
+        char[] ipAddress = new char[100];
+        int offset = 0;
+        int charsRead = 0;
+        BufferedReader br = null;
+        
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream("serveripaddress.txt")));
+            while ((charsRead = br.read(ipAddress, offset, ipAddress.length - offset)) != -1) {
+                offset += charsRead;
+                if (offset >= ipAddress.length) {
+                    break;
+                }
+            }
+            //work with IP address
+        } finally {
+            Arrays.fill(ipAddress,  (byte) 0);
+            br.close();
+        }
     }
 }
