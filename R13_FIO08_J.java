b03c2d2370520c65b09a455b22a26f42c3c84a15
sophia <sophiagnc@gmail.com>
2024-10-06 20:56:23 -0400

    update code to be compliant

--- R13_FIO08_J.java
+++ R13_FIO08_J.java
@@ -1,11 +1,13 @@
 //Rule 13 Input Output
 //FIO08-J Distinguish between characters or bytes read from a stream and -1
 
-//Non-Compliant Code:
+//Compliant Code:
 //...
 FileInputStream in;
 // Initialize stream
+int inbuff;
 byte data;
-while ((data = (byte) in.read()) != -1) {
-    // ...
+while ((inbuff = in.read()) != -1) {
+    data = (byte) inbuff;
+    // ... 
 }
