781d316ba6b15b85b9f69d2dfe562cb7ae4c87d1
sophia <sophiagnc@gmail.com>
2024-10-06 20:45:11 -0400

    update code to be compliant

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,11 +1,14 @@
 //Rule 02 Expressions
 //XP00-J Do not ignore values returned by methods 
 
-//Non-Compliant Code:
+//Compliant Code:
 //...
 public void deleteFile() {
     
     File someFile = new File ("someFileName.txt");
     //do something with someFile
-    someFile.delete();
+    
+    if (!someFile.delete()) {
+        // Handle failure to delete the file
+    }
 }
